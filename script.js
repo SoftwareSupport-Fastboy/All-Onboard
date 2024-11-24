4d984c598e4e065712a95625ea5f95f53682de5d
SoftwareSupport-Fastboy <fastsoftcanbe@gmail.com>
2024-11-24 22:31:06 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// đăng ký the service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch((error) => {
+            console.log('Service Worker registration failed:', error);
+        });
+    });
+}
+
+
+
 // Global variable to store the data
 let allData = [];
 let countdownTimer;
